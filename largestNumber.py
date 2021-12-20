497c055a3c3d1a840d67c0ee7898e131b2ab1e2f
bsreenua6 <sreenivasb37@gmail.com>
2021-12-20 18:04:18 +0530

    Create largestNumber.py

---
+++ largestNumber.py
@@ -0,0 +1,7 @@
+largest_so_for=0
+print('Start', largest_so_for)
+for num in [9, 41, 3, 12, 74, 15] :
+   if num > largest_so_for :
+      largest_so_for = num
+   print(largest_so_for, num)
+print('End', largest_so_for)
