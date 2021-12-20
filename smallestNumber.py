54ef233ed8f0c775c5577bc6dca22db78fc653a1
bsreenua6 <sreenivasb37@gmail.com>
2021-12-20 22:04:24 +0530

    Create smallestNumber.py

---
+++ smallestNumber.py
@@ -0,0 +1,11 @@
+# Program to find smallest number in an array of values
+smallest=None
+print('Start', largest_so_for)
+for num in [9, 41, 3, 12, 74, 15] :
+   if smallest is None :
+      smallest = num
+   elif num < smallest :
+      smallest = num
+   print(smallest, num)
+print('End', smallest)
+  
